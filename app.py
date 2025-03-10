5f004956bef897e8e22393ae6ec37483d827c00a
Vinicius Perrone <perronevinicius2018@gmail.com>
2025-03-10 19:47:57 -0300

    feat: added db instance

--- app.py
+++ app.py
@@ -1,13 +1,18 @@
 from flask import Flask
 
+from config.db import db
+
 
 def initialize_app():
     app = Flask(__name__)
+
+    db.init_app(app)
 
     return app
 
 if __name__ == "__main__":
     app = initialize_app()
+
 
     app.run(
         host="127.0.0.1",
