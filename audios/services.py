a3b8d21967f3b41201feab98eed16ffff210013c
Vinicius Perrone <perronevinicius2018@gmail.com>
2025-03-11 19:24:05 -0300

    feat: created TranscriptionService

--- audios/services.py
+++ audios/services.py
@@ -1,8 +1,13 @@
 import os
+from datetime import datetime as dt
+
+import openai
 
 from werkzeug.utils import secure_filename
 from utils.helpers_files import UPLOAD_FOLDER
 
+
+openai.api_key = os.getenv("OPENAI_API_KEY")
 
 class AudioSaveService():
 
@@ -27,3 +32,27 @@ class AudioSaveService():
         self.file.save(filepath)
 
         return filepath
+
+class TranscriptionService:
+    def __init__(self, file_path, filename):
+        self.file_path = file_path
+        self.filename = filename
+
+    def transcribe_audio(self):
+        try:
+            with open(self.file_path, "rb") as audio_file:
+                response = openai.Audio.transcribe("whisper-1", audio_file)
+
+            transcription_text = response["text"]
+
+            transcription_data = {
+                "filename": self.filename,
+                "transcription": transcription_text,
+                "status": "REALIZED",
+                "created_at": dt.utcnow()
+            }
+
+            return transcription_data
+
+        except Exception as e:
+            return None
