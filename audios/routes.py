b08a70c06cbf67e5b195a73fd1a696705834f192
Vinicius Perrone <perronevinicius2018@gmail.com>
2025-03-11 23:58:45 -0300

    feat: added transcriptions routes

--- audios/routes.py
+++ audios/routes.py
@@ -1,8 +1,13 @@
 from flask import Blueprint
 
-from audios.controllers import upload_file
+from audios.controllers import upload_file, list_transcriptions, detail_transcription, update_transcription, delete_transcription
 
 
 audios_blueprint = Blueprint('audios', __name__)
 
 audios_blueprint.add_url_rule('/audios/upload', view_func=upload_file, methods=['POST'])
+
+audios_blueprint.add_url_rule('/transcriptions/list', view_func=list_transcriptions, methods=['GET'])
+audios_blueprint.add_url_rule('/transcriptions/<int:user_id>/', view_func=detail_transcription, methods=['GET'])
+audios_blueprint.add_url_rule('/transcriptions/<int:user_id>/', view_func=update_transcription, methods=['PUT'])
+audios_blueprint.add_url_rule('/transcriptions/<int:user_id>/', view_func=delete_transcription, methods=['DELETE'])
