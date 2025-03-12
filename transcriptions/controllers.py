2c01144687a0831e5e2d58f474aba9878e868528
Vinicius Perrone <perronevinicius2018@gmail.com>
2025-03-12 05:33:52 -0300

    docs: implement swagger documentation in transcriptions

--- transcriptions/controllers.py
+++ transcriptions/controllers.py
@@ -1,5 +1,6 @@
 from flask import request, jsonify
 from flask_jwt_extended import jwt_required
+from flasgger import swag_from
 
 from bson.objectid import ObjectId
 
@@ -9,6 +10,20 @@ from transcriptions.models import TranscriptionModel
 from transcriptions.schemas import TranscriptionSchema
 
 
+@swag_from({
+    'tags': ['Gestão de Transcrições'],
+    'summary': 'Lista de todas as transcrições paginadas',
+    'description': 'Parâmatros opcionais de paginação `page` e `per_page`.',
+    'parameters': [
+        {'name': 'page', 'in': 'query', 'type': 'integer', 'required': False, 'description': 'Número da página para a paginação'},
+        {'name': 'per_page', 'in': 'query', 'type': 'integer', 'required': False, 'description': 'Número de itens por página'}
+    ],
+    'responses': {
+        200: {'description': 'List all successful transcriptions'},
+        401: {'description': 'Missing Token'},
+        500: {'description': 'Internal server error'}
+    }
+})
 @jwt_required()
 def list_transcriptions():
     page = request.args.get('page', 1, type=int)
@@ -31,6 +46,21 @@ def list_transcriptions():
         'transcriptions': transcriptions_schema.dump(transcriptions),
     }), 200
 
+
+@swag_from({
+    'tags': ['Gestão de Transcrições'],
+    'summary': 'Detalhe de Transcrição',
+    'description': 'Buscar detalhe de Transcrição por id.',
+    'parameters': [
+        {'name': 'transcription_id', 'in': 'path', 'type': 'string', 'required': True, 'description': 'ID da Transcrição'},
+    ],
+    'responses': {
+        200: {'description': 'Transcription found successful'},
+        401: {'description': 'Missing Token'},
+        404: {'description': 'Transcription not found'},
+        500: {'description': 'Internal server error'}
+    }
+})
 @jwt_required()
 def detail_transcription(transcription_id):
     transcription = TranscriptionModel.get_transcription_by_id(transcription_id)
@@ -41,6 +71,33 @@ def detail_transcription(transcription_id):
 
     return jsonify(transcription_schema.dump(transcription)), 200
 
+@swag_from({
+    'tags': ['Gestão de Transcrições'],
+    'summary': 'Atualizar Transcrição',
+    'description': 'Para atualizar transcrição especifíca, necessário informar o id',
+    'parameters': [
+        {'name': 'transcription_id', 'in': 'path', 'type': 'string', 'required': True, 'description': 'ID da Transcrição'},
+        {
+            'in': 'body',
+            'name': 'body',
+            'required': True,
+            'schema': {
+                'type': 'object',
+                'properties': {
+                    'transcription_text': {'type': 'string'},
+                    'status': {'type': 'string'},
+                }
+            }
+        },
+    ],
+    'responses': {
+        200: {'description': 'Transcription updated  successful'},
+        400: {'description': 'Invalid Data'},
+        401: {'description': 'Missing Token'},
+        404: {'description': 'Transcription not found'},
+        500: {'description': 'Internal server error'}
+    }
+})
 @jwt_required()
 def update_transcription(transcription_id):
     transcription = TranscriptionModel.get_transcription_by_id(transcription_id)
@@ -62,6 +119,21 @@ def update_transcription(transcription_id):
     except Exception as e:
         return jsonify({"message": "Internal Server Error", "error": str(e)}), 500
 
+
+@swag_from({
+    'tags': ['Gestão de Transcrições'],
+    'summary': 'Exclusão de Transcrição',
+    'description': 'Excluir transcrição por id.',
+    'parameters': [
+        {'name': 'transcription_id', 'in': 'path', 'type': 'string', 'required': True, 'description': 'ID da Transcrição'},
+    ],
+    'responses': {
+        201: {'description': 'Transcription deleted successfully'},
+        401: {'description': 'Missing Token'},
+        404: {'description': 'Transcription not found'},
+        500: {'description': 'Internal server error'}
+    }
+})
 @jwt_required()
 def delete_transcription(transcription_id):
     transcription = TranscriptionModel.get_transcription_by_id(transcription_id)
@@ -72,11 +144,27 @@ def delete_transcription(transcription_id):
     try:
         TranscriptionModel.collection.delete_one({"_id": ObjectId(transcription_id)})
 
-        return jsonify({"message": "Transcript deleted successfully"}), 201
+        return jsonify({"message": "Transcription deleted successfully"}), 201
 
     except Exception as e:
         return jsonify({"message": "Internal Server Error"}), 201
 
+
+@swag_from({
+    'tags': ['Filtro de Transcrições'],
+    'summary': 'Buscar transcrições por texto',
+    'description': 'Pesquisa por parametro `query` e parâmatros opcionais de paginação `page` e `per_page`.',
+    'parameters': [
+        {'name': 'query', 'in': 'query', 'type': 'string', 'required': True, 'description': 'Texto para pesquisa'},
+        {'name': 'page', 'in': 'query', 'type': 'integer', 'required': False, 'description': 'Número da página'},
+        {'name': 'per_page', 'in': 'query', 'type': 'integer', 'required': False, 'description': 'Número de items por página'}
+    ],
+    'responses': {
+        200: {'description': 'List all successful transcriptions'},
+        401: {'description': 'Missing Token'},
+        500: {'description': 'Internal server error'}
+    }
+})
 @jwt_required()
 def search_transcriptions_by_query():
     query = request.args.get("query", None)
